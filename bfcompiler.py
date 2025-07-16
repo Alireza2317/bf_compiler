60ee132730a242f1d7bf52e73ed0083c943d3a1c
Alireza <alireza.alavi237@gmail.com>
2025-07-16 04:15:08 +0330

    still trying😭

--- bfcompiler.py
+++ bfcompiler.py
@@ -1,27 +1,32 @@
 import sys
-from collections import deque
+
 TAPE_MAXLEN = 10_000
 
 class BFCompiler:
 	def	__init__(self) -> None:
 		self.set_code()
 		self.init_tape()
-		self.head: int = 0
-		self.code_end_indices: list[int] = []
-		self.code_start_indices: list[int] = []
-		self.abs_code_index: int = 0
+
+		#self.code_end_indices: list[int] = []
+		#self.code_start_indices: list[int] = []
+
+		self.code_pieces: list[str] = []
 
 	def init_tape(self) -> None:
-		self.tape: deque = deque(maxlen=TAPE_MAXLEN)
+		self.tape: list[int] = []
 
 		for _ in range(TAPE_MAXLEN):
 			self.tape.append(0)
 
-	def set_code(self) -> None:
-		raw: str = self.read_file()
-		self.code: str = self.strip_code(raw)
+		self.head: int = 0
 
-	def read_file(self) -> str:
+	def set_code(self) -> None:
+		raw: str = self._read_file()
+		self.code: str = self._strip_code(raw)
+
+		self.code_index: int = 0
+
+	def _read_file(self) -> str:
 		try:
 			#code_path: str = sys.argv[1]
 			code_path: str = 'test.bf'
@@ -37,7 +42,7 @@ class BFCompiler:
 
 		return raw_code
 
-	def strip_code(self, raw_code: str) -> str:
+	def _strip_code(self, raw_code: str) -> str:
 		code: str = ''
 		for char in raw_code:
 			if char in '[<+-.,>]':
@@ -45,42 +50,48 @@ class BFCompiler:
 
 		return code
 
-	def _find_end_bracket(self, current_index: int, code: str) -> int:
+	def _find_end_bracket(self, code: str, current_index: int) -> int:
+		if code[current_index] != '[':
+			raise ValueError('Loop does not start here!')
+
+		current_index += 1
+
 		openings_count: int = 0
-		for i in range(current_index+1, len(code)):
-			if code[i] == '[':
+		while current_index < len(code):
+			if code[current_index] == '[':
 				openings_count += 1
-			elif code[i] == ']':
+			elif code[current_index] == ']':
 				if openings_count == 0:
-					return i
+					return current_index
 				openings_count -= 1
+
+			current_index += 1
 
 		raise SyntaxError('Unbalanced Brackets!')
 
+	def add_code_loop(self) -> None:
+		pass
+
 	@property
-	def current_cell(self):
+	def current_code_char(self) -> str:
+		return self.code[self.code_index]
+
+	@property
+	def current_cell(self) -> int:
 		return self.tape[self.head]
 
-	def run(self, code: str | None = None):
+	def run(self, code: str | None = None) -> None:
 		if code is None:
 			code = self.code
 
 		code_index: int = 0
+
 		while code_index < len(code):
 			match code[code_index]:
 				case '[':
-					self.code_start_indices.append(code_index)
-					end_i: int = self._find_end_bracket(code_index, code)
-					self.code_end_indices.append(end_i)
-					self.run(code[code_index+1:end_i+1])
+					...
 				case ']':
-
-					if self.current_cell != 0:
-						code_index = self.code_start_indices[-1] + 1
-
-					else: # loop ends
-						self.code_start_indices.pop()
-						code_index = self.code_end_indices.pop()
+					...
 
 				case '>':
 					self.head += 1
@@ -97,9 +108,11 @@ class BFCompiler:
 				case ',':
 					self.tape[self.head] = ord(input()[0])
 
-			code_index += 1
+			self.code_index += 1
 
 
 if __name__ == '__main__':
 	compiler = BFCompiler()
+
+
 	compiler.run()
